e05c5ba522a1f19bb461154635caf96209b0ad16
saikiaki <saikiaki@mori-system.co.jp>
2017-01-05 13:25:47 +0900

    ソートするように変更

--- main.go
+++ main.go
@@ -4,35 +4,81 @@ import "bufio"
 import "fmt"
 import "github.com/mitchellh/go-homedir"
 import "github.com/urfave/cli"
+import "io"
+import "log"
 import "os"
+import "os/user"
 import "path/filepath"
+import "sort"
 
 var out = "~/.pin"
 
+func init() {
+	u, err := user.Current()
+	if err != nil {
+		log.Panic(err)
+	}
+	out = filepath.Join(u.HomeDir, ".pin")
+}
+
+type empty struct{}
+type list map[string]empty
+
+func (l list) add(s string) {
+	l[s] = empty{}
+}
+
+func (l list) list() []string {
+	keys := make([]string, 0, len(l))
+	for k := range l {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
 func main() {
+	log.Print("main")
 	app := cli.NewApp()
 	app.Name = "pin"
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "file, f",
-			Value: out,
-			Usage: "in/out file.",
-		},
-	}
 	app.Version = "1.0.0"
-	app.Action = func(c *cli.Context) error {
-		out = c.String("file")
-		if !c.Args().Present() {
-			list()
-			return nil
-		}
-		err := appendList(c.Args().First())
-		if err != nil {
-			return err
-		}
+	app.Action = action
+	app.Run(os.Args)
+}
+
+func action(c *cli.Context) error {
+	if !c.Args().Present() {
+		show()
 		return nil
 	}
-	app.Run(os.Args)
+	err := add(c.Args().First())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func add(path string) error {
+	path, err := format(path)
+	if err != nil {
+		return nil
+	}
+	l, err := read()
+	if err != nil {
+		return nil
+	}
+	l.add(path)
+	w, err := open(out)
+	defer w.Close()
+	if err != nil {
+		return err
+	}
+	o := l.list()
+	sort.Strings(o)
+	err = write(w, o)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func expand(path string) (string, error) {
@@ -52,56 +98,62 @@ func open(path string) (*os.File, error) {
 	if err != nil {
 		return nil, err
 	}
-	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
 		return nil, err
 	}
 	return file, nil
 }
 
-func list() error {
-	file, err := open(out)
-	defer file.Close()
+func show() error {
+	l, err := read()
 	if err != nil {
-		return err
+		return nil
 	}
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+	for _, v := range l.list() {
+		fmt.Println(v)
 	}
 	return nil
 }
 
-func appendList(path string) error {
+func format(path string) (string, error) {
 	path, err := expand(path)
 	if err != nil {
-		return err
+		return "", err
 	}
 	stat, err := os.Stat(path)
 	if err != nil {
-		return err
+		return "", err
 	}
 	if stat.IsDir() {
 		path = filepath.Clean(path) + string(filepath.Separator)
 	}
 	if _, err = os.Stat(path); err != nil {
-		return err
+		return "", err
 	}
+	return path, err
+}
+
+func read() (a list, err error) {
+	a = make(list)
 	file, err := open(out)
 	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		v := scanner.Text()
-		if v == path {
-			// duplicate.
-			return nil
-		}
-	}
-	_, err = file.WriteString(path + "\n")
 	if err != nil {
-		return err
+		return
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		a.add(scanner.Text())
+	}
+	return
+}
+
+func write(w io.Writer, s []string) error {
+	for _, v := range s {
+		_, err := fmt.Fprintln(w, v)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	return nil
 }
